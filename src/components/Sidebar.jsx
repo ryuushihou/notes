039b789e3bf58dfbdcea35fae89f7eec24dbf6f2
liuzhipeng <liuzhipengdongjing@gmail.com>
2024-09-15 00:22:58 +0900

    file upload

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,7 @@ import SidebarSearchField from '@/components/SidebarSearchField';
 import SidebarNoteList from '@/components/SidebarNoteList';
 import EditButton from '@/components/EditButton';
 import NoteListSkeleton from '@/components/NoteListSkeleton';
+import SidebarImport from '@/components/SidebarImport';
 
 export default async function Sidebar() {
     return (
@@ -24,6 +25,7 @@ export default async function Sidebar() {
                         <SidebarNoteList />
                     </Suspense>
                 </nav>
+                <SidebarImport />
             </section>
         </>
     )
