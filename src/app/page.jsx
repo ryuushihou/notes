337f05dbb8d6fecf494afb8b1e0ffba36fe3e48c
liuzhipeng <liuzhipengdongjing@gmail.com>
2024-10-11 00:35:15 +0900

    第一次

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,6 @@ export default async function Page() {
       <span className="note-text--empty-state">
         Click a note on the left to view something!123 🥺
         abc 🥺
-          nihao🥺
       </span>
     </div>
   )
